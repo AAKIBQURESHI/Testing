2ecfbcf7accf29fd93c079363fc5d458fa6569fd
AAKIBQURESHI <AAKIB.1760@GMAIL.COM>
2017-05-23 20:27:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     FB_PAGE_TOKEN: 'EAAC5Ahz0SZBYBAAT21BgzzeIKvJu8Jz1sNCnZAv9RlJ9znIM1LZAS0kryw9VgjgZCl4G9gODe7gYhHg0PRBVr2zPKtuszEmF6NtHTpzzd5Jjh3udfCuXOKZC3hlilhABbt5MqYWbNK5mtjxqZBcws8e6oQTB5zoOHZCd0neKWNovwZDZD',
-    FB_VERIFY_TOKEN: 'infoBeans',
+    FB_VERIFY_TOKEN: 'Indoremerijaan0731',
     API_AI_CLIENT_ACCESS_TOKEN: '59115751b0224866a2e31a653bf4e241',
     FB_APP_SECRET: '3918a116cb5bf494263d5205556b9056',
     SERVER_URL: "https://aakibqureshi.herokuapp.com/",
